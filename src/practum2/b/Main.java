878cc249e7e2d1a5a2fea604818134527e97b12c
jurre <jurre.bosman@live.nl>
2022-02-15 13:19:57 +0100

    Practicum 2B class en main

---
+++ src/practum2/b/Main.java
@@ -0,0 +1,20 @@
+package practum2.b;
+
+public class Main {
+    public static void main(String[] args) {
+        Voetbalclub ajx = new Voetbalclub("Ajax      ");
+        Voetbalclub feij = new Voetbalclub("Feijenoord");
+
+        feij.verwerkResultaat('w');
+        feij.verwerkResultaat('w');
+        feij.verwerkResultaat('w');
+        feij.verwerkResultaat('g');
+
+        System.out.println("Feijenoord punten: " + feij.aantalPunten());
+        System.out.println("Ajax gespeeld: " + ajx.aantalGespeeld());
+        System.out.println();
+
+        System.out.println(ajx);
+        System.out.println(feij);
+    }
+}
---
+++ src/practum2/b/Voetbalclub.java
@@ -0,0 +1,58 @@
+package practum2.b;
+
+public class Voetbalclub {
+    private String naam;
+    private int aantalGespeeld;
+    private int aantalGewonnen;
+    private int aantalGelijk;
+    private int aantalVerloren;
+
+    public Voetbalclub(String n) {
+        naam = n;
+
+
+    }
+
+    public void verwerkResultaat(char ch) {
+        if (ch == 'w')
+            aantalGewonnen = aantalGewonnen + 1;
+        if (ch == 'g')
+            aantalGelijk = aantalGelijk + 1;
+        if (ch == 'v')
+            aantalVerloren = aantalVerloren + 1;
+    }
+
+    public int aantalGespeeld() {
+        int aantal = aantalGewonnen + aantalVerloren + aantalGelijk;
+        return aantal;
+
+    }
+
+    public int getAantalGespeeld() {
+        return aantalGespeeld;
+    }
+
+    public int getAantalGewonnen() {
+        return aantalGewonnen;
+    }
+
+    public int getAantalGelijk() {
+        return aantalGelijk;
+    }
+
+    public int getAantalVerloren() {
+        return aantalVerloren;
+    }
+
+    public String toString() {
+        String s = naam + " " + aantalGespeeld() + " " + aantalGewonnen + " " + aantalGelijk + " " + aantalVerloren + " " + aantalPunten();
+        return s;
+    }
+
+    public int aantalPunten() {
+        int p = (aantalGewonnen * 3) + (aantalGelijk * 1);
+        return p;
+
+
+    }
+}
