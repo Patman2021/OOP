30f80e982fe5171e08cde16a281a572f186507a5
jurre <jurre.bosman@live.nl>
2022-02-15 08:48:50 +0100

    Practicum 2 class

---
+++ src/practum2/Zwembad.java
@@ -0,0 +1,33 @@
+package practum2;
+
+public class Zwembad {
+    private double breedte;
+    private double lengte;
+    private double diepte;
+
+    public Zwembad ( double br, double l, double d){
+        breedte= br;
+        lengte= l;
+        diepte= d;
+
+    }
+    public  void setBreedte(double br) {breedte= br;}
+    public  void setLengte(double l) {lengte= l;}
+    public  void setDiepte(double d) {diepte= d;}
+    public double getBreedte() { return breedte;}
+    public double getLengte() {return lengte;}
+    public double getDiepte() {return diepte;}
+    public String toString() {
+    String s="  Het zwembad een een breedte van "+ breedte+"m. Met een lengte van "+lengte +"m en een diepte van "+ diepte+"m";
+    return s;
+    }
+
+    public  double inhoud(){
+         double inhoud= breedte * lengte * diepte;
+
+        return inhoud;
+    }
+
+}
+
+
